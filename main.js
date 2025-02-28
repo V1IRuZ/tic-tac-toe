78b28b2a890cb05d6364d2495fce9d63bc623067
Esa <esa-pitkanen@hotmail.com>
2025-02-28 19:40:53 +0200

    Add a function for flow of the game
    The Gamecontroller function controls the flow of the game. It added player objects, player switch, active player and game round functions. The gameboard module contains a 2D array, and a function that places a marker on the board.

--- main.js
+++ main.js
@@ -3,58 +3,82 @@ const Gameboard = (function () {
     const rows = 3;
     const columns = 3;
     const gameBoard = [];
-    let cell = ""
 
     for (let i = 0; i < rows; i++) {
         gameBoard[i] = [];
         for (let j = 0; j < columns; j++) {
-            gameBoard[i].push(cell);
+                gameBoard[i].push("");     
         }        
     }
 
     const getGameBoard = () => gameBoard;
-    
+
+    const setMarker = (player) => {
+        const rowValue = +prompt("Pick a row (between 0 - 2) ");
+        const columnValue = +prompt("Pick a columns between 0 - 2");
+        const markerValue = player.marker
+        
+            let markedSpot = gameBoard[rowValue][columnValue];
+            if(!markedSpot) {
+                return gameBoard[rowValue][columnValue] = markerValue;
+                
+            } else {
+                console.log("Spot has been used ")
+            }  
+    } 
+
     return {
-        getGameBoard
+        getGameBoard,
+        setMarker
     }
 })();
 
 // Create player objects
-const players = (function () {
 
-    const playerOne = function (name) {
-        const marker = "X";
-        
-        return {
-            name,
-            marker
-        }
+
+// Playround
+
+function Gamecontroller () {
+   
+    const playerOneName = "Mike"
+    const playerTwoName = "John"
+
+    const playerOne = {
+        name: playerOneName,
+        marker: "X"
     }
 
-    const playerTwo = function (name) {
-        const marker = "O";
-
-        return {
-            name, 
-            marker
-        }
+    const playerTwo = {
+        name: playerTwoName,
+        marker: "O"
     }
 
-    const player1 = playerOne("Mike");
-    const player2 = playerTwo("John");
+    const player1 = playerOne
+    const player2 = playerTwo
 
-    const getPlayers = () => {
-        return  {
-            player1,
-            player2
-        }
+    let activePlayer = player1;
+
+    const switchPlayer = () => {
+        return activePlayer = activePlayer === player1 ? player2 : player1; 
     }
 
+    const getActivePlayer = () => activePlayer;
+
+    const {setMarker, getGameBoard} = Gameboard;
+
+    
+    const playRound = () => {
+        setMarker(activePlayer);
+    }
+    
+
+ 
     return {
-        getPlayers
+       getActivePlayer,
+       playRound
     }
+}
 
-})();
 
 
 // Take player inputs
