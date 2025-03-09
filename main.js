4fcd9a46ad0611340186c629fe554565d3ee30a3
Esa <esa-pitkanen@hotmail.com>
2025-03-09 20:20:24 +0200

    Allow multiple rounds of play
    The game can now be played in multiple rounds, and users' scores are updated as the rounds progress. With the Next Round button, users can continue playing for as long as they want, and this also resets the game board at the end of the round.

--- main.js
+++ main.js
@@ -20,11 +20,14 @@ const Gameboard = (function () {
     }
 
     const resetGameBoard = () => {
-        for (let i = 0; i < gameBoard.length; i++) {
-            for (let j = 0; j < gameBoard[i].length; j++) {
-                gameBoard[i][j] = "";
+
+        if (resetBoard) {
+            for (let i = 0; i < gameBoard.length; i++) {
+                for (let j = 0; j < gameBoard[i].length; j++) {
+                    gameBoard[i][j] = "";
+                }
             }
-         }
+        }  
     }
 
     return {
@@ -39,36 +42,50 @@ function Gamecontroller () {
    
     const playerOneName = "Mike"
     const playerTwoName = "John"
-    const board = Gameboard.getGameBoard();
+
+    const {getGameBoard, resetGameBoard} = Gameboard
+    const board = getGameBoard();
     
     const playerOne = {
         name: playerOneName,
-        marker: "X"
+        marker: "X",
+        score: 0,
+        color: "red"
     }
 
     const playerTwo = {
         name: playerTwoName,
-        marker: "O"
+        marker: "O",
+        score: 0,
+        color: "blue"
     }
 
     const game = {
         running: true,
-        activePlayer: playerOne
+        activePlayer: playerOne,
+        rounds: 5
     }
 
     const switchPlayer = () => {
         game.activePlayer = game.activePlayer === playerOne ? playerTwo : playerOne;
     }
 
+    const getPlayerOne = () => playerOne;
+
+    const getPlayerTwo = () => playerTwo;
+
     const getActivePlayer = () => game.activePlayer;
 
     const getGameRunning = () => game.running;
+
+    const continueGame = (value) => {game.running = value};
 
     const checkWinningConditions = () => {
             for (let i = 0; i < board.length; i++) {
                 // Check rows 
                 const allRowsEqual = board[i].every(value => value === game.activePlayer.marker)
                 if(allRowsEqual) {
+                    game.activePlayer.score++
                     game.running = false
                     return console.log(`${game.activePlayer.name} is winner!`)
                 }
@@ -78,6 +95,7 @@ function Gamecontroller () {
                 columns.push(board[0][i], board[1][i], board[2][i])
                 const allColumnsEqual = columns.every(value => value === game.activePlayer.marker)
                 if (allColumnsEqual) {
+                    game.activePlayer.score++
                     game.running = false
                     return console.log(`${game.activePlayer.name} is winner!`)
                 }
@@ -88,6 +106,7 @@ function Gamecontroller () {
                 if (board[0][0] === game.activePlayer.marker && board[1][1] === game.activePlayer.marker && board[2][2] === game.activePlayer.marker || 
                     board[0][2] === game.activePlayer.marker && board[1][1] === game.activePlayer.marker && board[2][0] === game.activePlayer.marker) {
                     
+                    game.activePlayer.score++
                     game.running = false
                     return console.log(`${game.activePlayer.name} is winner!`)
                 }
@@ -106,60 +125,82 @@ function Gamecontroller () {
         checkWinningConditions();
         switchPlayer();
         console.log(Gameboard.getGameBoard());
-        } else {
-            console.log("Game over.")
-        }     
-    }
+        console.log(game.running);
+        } 
         
+        if (!game.running) {
+            resetGameBoard();
+        }
+         
+    }
+
     return {
+       getPlayerOne,
+       getPlayerTwo,
        getActivePlayer,
        getGameRunning,
+       continueGame,
        playRound
     }
 }
 
 const DisplayController = (function() {
     const squares = document.querySelectorAll(".square");
+    const player1Score = document.querySelector(".player1>p");
+    const player2Score = document.querySelector(".player2>p");
+    const nextRoundBtn = document.querySelector(".next-round");
 
     // Extract functions from Gameboard.
     const {getGameBoard, setMarker} = Gameboard
     const board = getGameBoard();
 
     // Extract functions from Gamecontroller.
-    const {getActivePlayer, playRound, getGameRunning} = Gamecontroller();
+    const {getPlayerOne, getPlayerTwo, getActivePlayer, playRound, getGameRunning, continueGame} = Gamecontroller();
+    const player1 = getPlayerOne();
+    const player2 = getPlayerTwo();
 
-    const render = () => {
-        let row;
-        let column;
-        
-        squares.forEach(square => {
-            square.addEventListener("click", (e) => {
+    // Display scoreboard
 
-                // Take the location values ​​from the attributes into the 2D array of the game board.
-                row = e.target.getAttribute("data-row");
-                column = e.target.getAttribute("data-column");
+    squares.forEach(square => {
+        square.addEventListener("click", (e) => {
 
-                const activePlayer = getActivePlayer();
-                const gameRunning = getGameRunning();
+            // Take the location values ​​from the attributes into the 2D array of the game board.
+            const row = e.target.getAttribute("data-row");
+            const column = e.target.getAttribute("data-column");
 
-                if (gameRunning) {
-                    // Check if the square on the game board is marked.
-                    if (!board[row][column]) {
-                        square.textContent = `${activePlayer.marker}`;
+            const activePlayer = getActivePlayer();
+            const gameRunning = getGameRunning();
 
-                         // Update game board array
-                        setMarker(activePlayer, row, column);
-                        playRound()
-                        
-                    } else {
-                        console.log("Spot has been used!");
-                    }
+            if (gameRunning) {
+                nextRoundBtn.disabled = true;
+                // Check if the square on the game board is marked.
+                if (!board[row][column]) {
+                    square.style.color = activePlayer.color;
+                    square.textContent = `${activePlayer.marker}`;
+
+                    // Update game board array
+                    setMarker(activePlayer, row, column);
+                    playRound()
+
+                } else {
+                    console.log("Spot has been used!");
                 }
-            })    
-        })
-    }
-  
-    render()
+            }
+
+            if (!gameRunning) {
+                nextRoundBtn.disabled = false;
+            }
+
+            player1Score.textContent = `${player1.name} score: ${player1.score}`;
+            player2Score.textContent = `${player2.name} score: ${player2.score}`;
+        })    
+    })
+
+ 
+    nextRoundBtn.addEventListener("click", () => {
+        squares.forEach(square => square.textContent = "");
+        continueGame(true);
+    })
 
 })();
 
